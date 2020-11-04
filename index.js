e12931664bd48990ae5b2638addb846f4a4ab376
JeffCrafty <73064053+JeffCrafty@users.noreply.github.com>
2020-11-04 23:46:36 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://brhood-music.glitch.me"]
+const urls = ["https://brhood-bot.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
