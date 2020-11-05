cc64831c3de2f9cafe5d6594eda09689e9c09c21
JeffCrafty <73064053+JeffCrafty@users.noreply.github.com>
2020-11-05 18:20:17 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const axios = require("axios")
 const urls = ["https://positive-aquamarine-cotija.glitch.me"]
+const urls = ["https://furry-swanky-stock.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
