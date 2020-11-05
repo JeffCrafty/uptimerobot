27a2b0b6c59223b82df2e518edc929a639288c37
JeffCrafty <73064053+JeffCrafty@users.noreply.github.com>
2020-11-05 17:36:54 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://brhood-bot.glitch.me"]
+const urls = ["https://positive-aquamarine-cotija.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
